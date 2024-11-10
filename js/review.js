4622e3228c4439bd4c32c1ef1166f9eb1620f49e
Rabia Kabir <139744497+22rabia@users.noreply.github.com>
2024-11-10 21:33:09 +0500

    Update review.js

--- js/review.js
+++ js/review.js
@@ -1,13 +1,3 @@
 
     const stars = document.querySelectorAll('.rating input[type="radio"]');
-    const ratingValue = document.querySelector('.rating');
-
-    stars.forEach((star) => {
-        star.addEventListener('click', function () {
-            ratingValue.setAttribute('data-rating', this.value);
-        });
-    });
-
-document.addEventListener("change",function(){
-    
-})
+  
